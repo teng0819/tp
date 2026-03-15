e36e019e2248da5b73835b83a335712ec4c7b20d
jai <vjaivasanth@gmail.com>
2026-03-15 21:01:53 +0800

    Check workflow

---
+++ src/test/java/seedu/address/logic/commands/ShowCommandTest.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code ShowCommand}.
+ */
+public class ShowCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+
+}
