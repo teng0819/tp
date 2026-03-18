093bd0af23a705db8bac02a0479bdda093633a17
Teng Lingjing <e1354629@u.nus.edu>
2026-03-18 23:56:37 +0800

    Fix checkstyle issues 4.0

--- src/test/java/seedu/address/logic/commands/MarkTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkTaskCommandTest.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class MarkTaskCommandTest {
 
