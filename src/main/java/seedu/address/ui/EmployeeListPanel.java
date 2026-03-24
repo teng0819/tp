f5cca5cfd657136d07341c160842dbbfca6c2d0c
jai <vjaivasanth@gmail.com>
2026-03-25 01:25:03 +0800

    Improve cosmetics of UI

--- src/main/java/seedu/address/ui/EmployeeListPanel.java
+++ src/main/java/seedu/address/ui/EmployeeListPanel.java
@@ -1,11 +1,17 @@
 package seedu.address.ui;
 
+import java.util.Set;
 import java.util.logging.Logger;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.employee.Employee;
@@ -27,6 +33,47 @@ public class EmployeeListPanel extends UiPart<Region> {
         super(FXML);
         employeeListView.setItems(personList);
         employeeListView.setCellFactory(listView -> new EmployeeListViewCell());
+
+        Platform.runLater(() -> {
+            ScrollBar verticalBar = getVerticalScrollBar(employeeListView);
+            if (verticalBar != null) {
+                employeeListView.addEventFilter(ScrollEvent.SCROLL, event -> {
+                    if (event.getDeltaY() != 0) {
+                        double scrollAmount = -event.getDeltaY() * 0.0008;
+                        double newValue = clamp(
+                                verticalBar.getValue() + scrollAmount,
+                                verticalBar.getMin(),
+                                verticalBar.getMax()
+                        );
+                        verticalBar.setValue(newValue);
+                        event.consume();
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * Returns the vertical scrollbar of the given ListView, if present.
+     */
+    private ScrollBar getVerticalScrollBar(ListView<?> listView) {
+        Set<Node> nodes = listView.lookupAll(".scroll-bar");
+        for (Node node : nodes) {
+            if (node instanceof ScrollBar) {
+                ScrollBar bar = (ScrollBar) node;
+                if (bar.getOrientation() == Orientation.VERTICAL) {
+                    return bar;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Clamps the value between min and max.
+     */
+    private double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
     }
 
     /**
