aa95ff8e8b817168deb8d3eef11358045344b0c7
Lin Muxi <e1355399@u.nus.edu>
2026-03-26 21:47:01 +0800

    Fix import order in help window content test

--- src/test/java/seedu/address/ui/HelpWindowContentTest.java
+++ src/test/java/seedu/address/ui/HelpWindowContentTest.java
@@ -8,8 +8,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.ui.HelpWindowContent.DisplayLine;
-import seedu.address.ui.HelpWindowContent.HelpSectionDisplay;
 import seedu.address.ui.HelpWindowContent.HelpSection;
+import seedu.address.ui.HelpWindowContent.HelpSectionDisplay;
 
 public class HelpWindowContentTest {
 
